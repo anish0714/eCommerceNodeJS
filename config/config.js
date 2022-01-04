28702827b038fca7fbf39f7b768f37dd1dba544b
Anish Dandekar <irock0714@gmail.com>
2022-01-04 11:20:35 -0500

    url

--- config/config.js
+++ config/config.js
@@ -1,6 +1,5 @@
 const config = {
-  MONGO_URI:
-    "mongodb+srv://admin:admin123@shoppingkart.86f71.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  MONGO_URI: "YOUR MONGODB URL",
   JWT_SECRET: "secret",
 };
 
